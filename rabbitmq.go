36fa2d3549807cfa4833f40cb7c39153b050dc91
Ervin <ervin@paperclicks.net>
2020-01-04 21:12:12 +0100

    changed rpc delete queue conditions

--- rabbitmq.go
+++ rabbitmq.go
@@ -490,7 +490,7 @@ func (rmq *RabbitMQ) PublishRPC2(qInfo QueueInfo, body string, headersTable amqp
 	response = <-replyToMessages
 
 	//delete the reply to queue after the response has been received
-	_, err = ch.QueueDelete(replyToQueue.Name, true, true, false)
+	_, err = ch.QueueDelete(replyToQueue.Name, false, true, false)
 	if err != nil {
 		log.Printf("ERROR: %s", err.Error())
 	}
